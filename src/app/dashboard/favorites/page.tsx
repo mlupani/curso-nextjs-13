598d2f218b2dd1da6c2bc9c53e27e3631b62573e
Miguel Lupani <mlupani2@gmail.com>
2024-08-19 14:13:53 -0300

    seccion 7.. instalando redux toolkit

---
+++ src/app/dashboard/favorites/page.tsx
@@ -0,0 +1,22 @@
+import { PokemonGrid, PokemonsReponse, SimplePokemon } from "@/pokemons";
+
+export const metadata = {
+    title: 'Favoritos',
+    description: 'Listado de pokemons',
+};
+
+
+
+
+export default async function PokemonsPage() {
+
+  return (
+    <div className="flex flex-col">
+
+      <span className="text-5xl my-2">Pokémons favoritos <small className="text-blue-500">Global State</small></span>
+      
+      <PokemonGrid pokemons={ [] } />
+
+    </div>
+  );
+}
--- src/app/dashboard/main/page.tsx
+++ src/app/dashboard/main/page.tsx
@@ -1,8 +1,14 @@
+import { SimpleWidget } from "@/app/components";
 
 export default function MainPage() {
   return (
     <div>
-      <h1>Hello Page Main</h1>
+      <h1 className="mt-2 text-3xl">Dashboard</h1>
+      <span className="text-xl">Informacion general</span>
+
+      <div className="flex flex-wrap p-2 items-center justify-center mt-4">
+        <SimpleWidget/>
+      </div>
     </div>
   );
 }
